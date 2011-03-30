957ac97ec7274a9c29e9d961a126da3a81a5b5cd
Nick Berardi <nick@managedfusion.com>
2011-03-30 11:06:47 -0400

    added mocks for http and mvc

---
+++ src/Mvc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+using System.Collections.Specialized;
+
+namespace ManagedFusion.Testing
+{
+	public static class Mvc
+	{
+		public static ControllerBase SetControllerContext(this ControllerBase controller)
+		{
+			var httpContext = Http.MockHttpContext();
+			var controllerContext = new ControllerContext(httpContext, new RouteData(), controller);
+			controller.ControllerContext = controllerContext;
+
+			return controller;
+		}
+
+		public static ControllerBase SetValueProvider(this ControllerBase controller, IDictionary<string, string> values = null)
+		{
+			if (values == null)
+				values = new Dictionary<string, string>();
+
+			var collection = new NameValueCollection();
+			foreach (var v in values)
+				collection.Add(v.Key, v.Value);
+
+			controller.ValueProvider = new NameValueCollectionValueProvider(collection, null);
+
+			return controller;
+		}
+	}
+}
